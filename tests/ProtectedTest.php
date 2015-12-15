0cd0a94dbd13306a159aac1875fe9073b40d18f0
toplan <toplan710@gmail.com>
2015-12-15 11:27:25 +0800

    add test

---
+++ tests/ProtectedTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Toplan\PhpSms\Sms;
+
+class ProtectedTest extends PHPUnit_Framework_TestCase
+{
+    public static function getPrivateMethod($name) {
+        $obj = new Sms();
+        $class = new ReflectionClass($obj);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
+
+    public function testConfiguration()
+    {
+        $method = self::getPrivateMethod('configuration');
+        $obj = new Sms();
+        $method->invokeArgs($obj, []);
+        $this->assertCount(1, Sms::getEnableAgents());
+        $this->assertCount(5, Sms::getAgentsConfig());
+    }
+
+    public function testValidator()
+    {
+        $method = self::getPrivateMethod('validator');
+        $obj = Sms::make()->to('18280000000');
+        $r = $method->invokeArgs($obj, []);
+        $this->assertTrue($r);
+    }
+}
