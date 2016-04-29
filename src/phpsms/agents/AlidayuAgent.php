c8f368a7fa0ae7b44589086a34d6ad7ceb0be2b8
BranchZero Sun <branchzero@gmail.com>
2016-04-29 23:34:46 +0800

    Fixed AlidayuAgent StyleCI

--- src/phpsms/agents/AlidayuAgent.php
+++ src/phpsms/agents/AlidayuAgent.php
@@ -29,14 +29,14 @@ class AlidayuAgent extends Agent
             'format'             => 'json',
             'sign_method'        => 'md5',
             'method'             => 'alibaba.aliqin.fc.sms.num.send',
-            'timestamp'          => date("Y-m-d H:i:s"),
+            'timestamp'          => date('Y-m-d H:i:s'),
             'sms_type'           => 'normal',
             'sms_free_sign_name' => $this->smsFreeSignName,
             'sms_param'          => json_encode($data),
             'rec_num'            => $to,
             'sms_template_code'  => $tempId,
         ];
-        $params["sign"] = $this->generateSign($params);
+        $params['sign'] = $this->generateSign($params);
         $result = $this->curl($sendUrl, $params, true);
         $this->genResult($result, 'alibaba_aliqin_fc_sms_num_send_response');
     }
@@ -50,12 +50,12 @@ class AlidayuAgent extends Agent
     {
         if ($result['response']) {
             $result = json_decode($result['response'], true);
-            if (isset($result[$callbackName]['result']) && $result[$callbackName]['result']['err_code'] == 0) {
+            if (isset($result[$callbackName]['result']) && $result[$callbackName]['result']['err_code'] == '0') {
                 $this->result['success'] = true;
                 return;
             } elseif (isset($result['error_response'])) {
                 $this->result['info'] = $result['error_response']['msg'] . '|sub_msg:' . $result['error_response']['sub_msg'] . '|result:' . json_encode($result ?: '');
-                $this->result['code'] = $result['error_response']['code'].'_'.$result['error_response']['sub_code'];
+                $this->result['code'] = $result['error_response']['code'] . '_' . $result['error_response']['sub_code'];
                 return;
             }
         }
@@ -68,7 +68,7 @@ class AlidayuAgent extends Agent
 
         $stringToBeSigned = $this->secretKey;
         foreach ($params as $k => $v) {
-            if (is_string($v) && "@" != substr($v, 0, 1)) {
+            if (is_string($v) && '@' !== substr($v, 0, 1)) {
                 $stringToBeSigned .= "$k$v";
             }
         }
